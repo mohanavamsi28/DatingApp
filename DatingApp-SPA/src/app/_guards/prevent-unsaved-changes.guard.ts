cbe29bd27162b9b3804b02aac4c3afa6cc0f443a
Vamsi Chappalli <mvamsichappalli@deloitte.com>
2019-01-25 15:10:40 +0530

    End of Section 10 - Saving edit profile to back end

---
+++ DatingApp-SPA/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Router, CanDeactivate } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreventUnsavedChanges implements CanDeactivate<MemberEditComponent> {
+  canDeactivate(component: MemberEditComponent): boolean {
+    if (component.editForm.dirty) {
+      return confirm('Are you sure you want to continue? Any unsaved changes will be lost!');
+    }
+    return true;
+  }
+}
